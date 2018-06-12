9c40aebe2be4c79d257444697e0b7dd0fb24e962
Alan Gibson <alan.gibson@gmail.com>
2018-06-12 22:24:06 +0200

    Saving content in macro body

--- src/store/modules/Confluence.js
+++ src/store/modules/Confluence.js
@@ -1,15 +1,37 @@
 import AP from 'atlassian-connect'
 
 const actions: any = {
-  cancel () {
+  cancel() {
     AP.confluence.closeMacroEditor()
   },
-  save (context: any) {
-    AP.confluence.saveMacro({
-      url: context.rootState.plantumlEditor.src,
-      encodedUML: context.rootState.plantumlEditor.encodedText
-    })
+  save(context: any) {
+    AP.confluence.saveMacro(
+      {
+        url: context.rootState.plantumlEditor.src,
+        // encodedUML: context.rootState.plantumlEditor.encodedText,
+        // bodyIsEncoded: false,
+        imageType: context.rootState.plantumlEditor.umlExtension
+      },
+      context.rootState.plantumlEditor.text)
     AP.confluence.closeMacroEditor()
+  },
+  loadParams(context: any, macroParams: any) {
+    if (macroParams.encodedUML) {
+      context.dispatch('plantumlEditor/renderEncodedUML', macroParams.encodedUML, {root: true})
+    }
+    // Set image type
+    if (macroParams.imageType) {
+      context.dispatch('plantumlEditor/setUmlExtension', macroParams.imageType, {root: true})
+      context.dispatch(
+        'plantumlEditor/renderUML',
+        context.rootState.plantumlEditor.text
+      )
+    }
+  },
+  loadBody(context: any, body: string) {
+    if (body && body !== '') {
+      context.dispatch('plantumlEditor/renderUML', body, {root: true})
+    }
   }
 }
 
