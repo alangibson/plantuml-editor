b58db082753c22c94c3ff13f021d460e5bee1f65
Alan Gibson <alan.gibson@gmail.com>
2018-07-11 07:41:12 +0200

    WIP: Dynamically building Plantuml url

--- src/views/ConfluenceMacro.vue
+++ src/views/ConfluenceMacro.vue
@@ -1,8 +1,36 @@
 <template>
-  <img id="ppu-img" :src="$route.query.url"/>
+  <img id="ppu-img" :src="url"/>
 </template>
 
 <script>
+import plantumlEncoder from 'plantuml-encoder-decoder'
+
 export default {
+  // TODO support dragscroll for image
+
+  data() {
+    return {
+      // TODO spinning-orb loading image?
+      url: this.$route.query.url
+    }
+  },
+  mounted() {
+    // TODO dont get url. Instead fetch macro body from Confluence Cloud, encode, then generate url
+
+    AP.request(
+      '/rest/api/content/' +
+        this.$route.query.pageId +
+        '/history/' +
+        this.$route.query.pageVersion +
+        '/macro/id/' +
+        this.$route.query.macroId
+    ).then((response: any) => {
+      let encodedUML: string = plantumlEncoder.encode(JSON.parse(response).body)
+      // TODO
+      // this.url = ??? + encodedUML
+      // TODO pass image type in url so we dont have to fetch it too
+      // this.$route.query
+    })
+  }
 }
 </script>
