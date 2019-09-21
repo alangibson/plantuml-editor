b1727a78b4e4bf73acdad7ea4fe49aeaa39b9f8f
Alan Gibson <alan.gibson@gmail.com>
2019-09-21 19:02:49 +0200

    Moving privacy and terms to own static files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 import VueRouter from 'vue-router'
 import Editor from '../views/Editor.vue'
-import Privacy from '../views/Privacy.vue'
 import ConfluenceEditor from '../views/ConfluenceEditor.vue'
 import ConfluenceMacro from '../views/ConfluenceMacro.vue'
 
 const routes = [
   { path: '/', component: Editor },
-  { path: '/privacy', component: Privacy },
   { path: '/confluence/editor', component: ConfluenceEditor },
   { path: '/confluence/macro', component: ConfluenceMacro }
 ]
